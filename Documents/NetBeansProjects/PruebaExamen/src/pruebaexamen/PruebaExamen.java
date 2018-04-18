0e7e8f886e5e3bd9073d95838d20f1ae6d56eb3a
Josue Amaya <josuetrejo999@gmail.com>
2018-04-17 22:38:09 -0600

    esta es la prueba examen en netbeas

---
+++ Documents/NetBeansProjects/PruebaExamen/src/pruebaexamen/PruebaExamen.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaexamen;
+
+import javax.swing.JOptionPane;
+
+/**
+ * nombre pruebaexamen
+ * fecha 17-04-18
+ * version 1.0
+ * Copyright Manada
+ * @authors josue
+ */
+public class PruebaExamen {
+
+    
+    public static void main(String[] args) {
+        
+        String num ;
+        int nombres[] = new int[4];
+        
+        
+        for (int i = 0; i < nombres.length ; i++) {
+            JOptionPane.showInputDialog("Ingresar la cantidad para la posicion"+i);
+            
+        }
+        
+        JOptionPane.showMessageDialog(null,"Buenale puto");
+        
+    }
+    
+}
